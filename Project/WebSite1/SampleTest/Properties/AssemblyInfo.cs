4d85730587e32d32d552dfa369888e7b1b4ce1b8
shivanisharma <shivani.silli@ionnor.com>
2010-12-23 16:08:13 +0530

    2nd commit

---
+++ Project/WebSite1/SampleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ION NOR Solutions Pvt. Ltd")]
+[assembly: AssemblyProduct("SampleTest")]
+[assembly: AssemblyCopyright("Copyright © ION NOR Solutions Pvt. Ltd 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a89225b7-9e74-4375-84de-3085c2dfda67")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project/WebSite1/SampleTest/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selenium;
+
+namespace SampleTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass,ignore]
+    public class UnitTest1
+    {
+        private ISelenium selenium;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            selenium = new DefaultSelenium("localhost", 4444, "*iexplore", "http://www.google.com/");
+            selenium.Start();
+
+            selenium.Open("/");
+            selenium.Type("q", "selenium rc");
+            selenium.Click("btnG");
+            selenium.WaitForPageToLoad("30000");
+            Assert.AreEqual("selenium rc - Google Search", selenium.GetTitle());
+
+            try
+            {
+                selenium.Stop();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+
+
+
+        }
+        
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string URL = "http://www.abb.com";
+            selenium = new DefaultSelenium("localhost", 4444, "*firefox", URL);
+            selenium.Start();
+            selenium.Open("/");
+            Assert.AreEqual("The ABB Group - Automation and Power Technologies", selenium.GetTitle());
+            selenium.Type("searchInput", "Robot");
+            selenium.Click("search");
+            selenium.WaitForPageToLoad("10000");
+            Assert.AreEqual("The ABB Group", selenium.GetTitle());
+            Assert.AreEqual("Robot", selenium.GetValue("searchInput"));
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            selenium = new DefaultSelenium("localhost", 4444, "*iexplore", "http://localhost:1987");
+            selenium.Start();
+            selenium.Open("/WebSite1/");
+            selenium.Type("MainContent_TextBox1", "HUM");
+            selenium.Click("MainContent_Button1");
+            selenium.WaitForPageToLoad("3000770");
+            
+            //Get the Table Value of Row 1 and Coulumn 2
+            object GG = selenium.GetTable("MainContent_GridView1.1.2");
+            
+            Assert.AreEqual("1", "1");
+            try
+            {
+                selenium.Stop();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+
+
+
+        }
+    }
+}
